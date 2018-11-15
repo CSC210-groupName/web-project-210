90ff69dfeefae13ea811e0fff7bdadaa156f95d8
Matthew Sundberg <matthew@mws.is>
2018-11-15 05:39:50 -0500

    basic database connection working now

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
+var express = require('express');
+var fs = require('fs');
+
+var secrets = JSON.parse(fs.readFileSync('./secrets.json', 'utf-8'));
 
 
-// Set up connection
+// Set up database connection
 var connection = mysql.createConnection({
+	// TO set up to work with password, need to get into mysql shell after installing it separately and run:
+	// Create USER 'XXXX'@'localhost' IDENTIFIED WITH mysql_native_password BY 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 	host: 'localhost',
-	user: 'testing',
-	password: '12345678'
-
+	user: secrets.mysqlUsername,
+	password: secrets.mysqlPassword,
+	database: 
 });
 
 connection.connect(function(err){
