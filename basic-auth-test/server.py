e710faedeae6036828604089774b990252bc7244
Jack Venuti <sgfield15@gmail.com>
2018-11-15 11:36:02 +0000

    Implement sqlite database

--- basic-auth-test/server.py
+++ basic-auth-test/server.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, request
 from flask_cors import CORS
 from argon2 import PasswordHasher
+import sqlite3
 app = Flask(__name__)
 CORS(app)
 
@@ -21,10 +22,13 @@ def loginRequest():
     data = json.loads(request.data);
     userVal = data['username'];
     passVal = data['password'];
-    result = 'Username not found';
-    if userVal in validLogins:
-        result = 'Login successful' if verifyPassword(validLogins[userVal], passVal) else 'Incorrect password';     
+    try:
+        existingPassHash = getStoredPassForUser(userVal); #If this throws exception, there is no username, drop into except block
+        result = 'Login successful' if verifyPassword(existingPassHash, passVal) else 'Incorrect password';
+    except:
+        result = 'Username not found';
     return json.dumps({'result':result});
+
 
 def verifyPassword(saved_hash, input_password_attempt):
     try:
@@ -33,13 +37,69 @@ def verifyPassword(saved_hash, input_password_attempt):
     except:
         return False;
 
-# Here's the temporary 'database'. These hashes are for 'pass1' and 'pass2', respectively
-validLogins = {
-    'john':'$argon2id$v=19$m=102400,t=2,p=8$9Kzm/DEQghBwzas81kIKxg$Pu1bQL00PJkpPRmLU7Jigg',
-    'jane':'$argon2id$v=19$m=102400,t=2,p=8$AuWPF6SH8a4QmKefLeUnaQ$2WzzozzQofJJxBKOY53yqg'
-};
-ph = PasswordHasher();
+def storeUserAndPass(username, password):
+    try:
+        getStoredPassForUser(username);
+        usernameExists = True;
+        print('Username already exists in database!');
+    except:
+        usernameExists = False;
+    if not usernameExists:
+        conn = sqlite3.connect(sqlite_file);
+        c = conn.cursor();
+        c.execute("insert into users values ('{}', '{}')".format(username, password));
+        conn.commit();
+        conn.close();
+
+# This function throws an exception if the username is not found
+def getStoredPassForUser(username):
+    conn = sqlite3.connect(sqlite_file);
+    c = conn.cursor();
+    c.execute("select password from users where username = '{}'".format(username));
+    result = c.fetchall()[0][0];
+    conn.close();
+    return result;
+
+def removeUser(username):
+    conn = sqlite3.connect(sqlite_file);
+    c = conn.cursor();
+    c.execute("delete from users where username = '{}'".format(username));
+    conn.commit();
+    conn.close();
+
+
+def initDB():
+    try:
+        conn = sqlite3.connect(sqlite_file);
+        conn.close();
+        success = True;
+    except Exception as e:
+        print(e);
+        print("Invalid db path");
+        success = False;
+    if success:
+        conn = sqlite3.connect(sqlite_file);
+        c = conn.cursor();
+        try:
+            c.execute("select * from users");
+        except:
+            c.execute("create table users (username TEXT, password TEXT)");
+            c.execute("select * from users");
+        finally:
+            print("Current State Of users Table:");
+            print(c.fetchall());
+            conn.close();
+    return success
+
+# These hashes are for 'pass1' and 'pass2', respectively
+# Run these commands (once) to enter the example data for a new db
+# storeUserAndPass('john','$argon2id$v=19$m=102400,t=2,p=8$9Kzm/DEQghBwzas81kIKxg$Pu1bQL00PJkpPRmLU7Jigg')
+# storeUserAndPass('jane','$argon2id$v=19$m=102400,t=2,p=8$AuWPF6SH8a4QmKefLeUnaQ$2WzzozzQofJJxBKOY53yqg')
 
 
 if __name__ == '__main__':
-	app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    sqlite_file = '/test_db.db';
+    ph = PasswordHasher();
+    dbInitialized = initDB();
+    if (dbInitialized):
+	    app.run(debug=True, port=5000) #run app in debug mode on port 5000
